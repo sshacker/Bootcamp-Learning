9513eb046a37f29a4fece69282506bf40cb3fcdc
Shivam Sonkar <shivam.sonkar@tothenew.com>
2022-06-30 11:49:47 +0530

    Spring boot starter project and autoconfiguration

---
+++ demo-spring-boot-project2/src/main/java/com/ttn/bootcamp/demospringbootproject2/courses/controller/CourseController.java
@@ -0,0 +1,17 @@
+package com.ttn.bootcamp.demospringbootproject2.courses.controller;
+
+import com.ttn.bootcamp.demospringbootproject2.courses.domain.Course;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @GetMapping("/courses")
+    public List<Course> getAllCourses() {
+        return Arrays.asList(new Course(1, "Java", "GeeksForGeeks"),new Course(2, "Python", "TutorialsPoint"));
+    }
+}
