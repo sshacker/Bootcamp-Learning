0eccc48275a422e2966949d8c995938dd1423b08
Shivam Sonkar <shivam.sonkar@tothenew.com>
2022-06-30 09:54:41 +0530

    spring-boot project2 initilizer downloaded

---
+++ demo-spring-boot-project2/src/main/java/com/ttn/bootcamp/demospringbootproject2/DemoSpringBootProject2Application.java
@@ -0,0 +1,13 @@
+package com.ttn.bootcamp.demospringbootproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootProject2Application.class, args);
+	}
+
+}
---
+++ demo-spring-boot-project2/src/test/java/com/ttn/bootcamp/demospringbootproject2/DemoSpringBootProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn.bootcamp.demospringbootproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
